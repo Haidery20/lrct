3778ca96e8ec48f33ce74ef655e3d4ef2ba82070
Haidery Shango <shangohaidery4@gmail.com>
2025-07-15 19:22:15 +0300

    fixing image issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
           <div className="flex justify-between items-center py-6">
             <div className="flex items-center space-x-3">
             <img 
-    src="public/images/Club Logo.svg" 
+    src="/images/Club Logo.svg" 
     alt="Club Logo" 
     className="h-10 w-10 object-contain transition-opacity duration-300"
   />
