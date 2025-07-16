1c28b792258e6b702f48860a0e5ef4e6e77bf006
Haidery Shango <shangohaidery4@gmail.com>
2025-07-16 23:49:58 +0300

    updated images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="lg:col-span-2">
             <div className="flex items-center space-x-3 mb-6">
             <img 
-    src="public/images/club_logo.svg" 
+    src="/images/club_logo.svg" 
     alt="Club Logo" 
     className="h-10 w-10 object-contain transition-opacity duration-300"
   />
